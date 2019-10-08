ea5f6e430a939347d1d12a390ba13ca4ef71a0b7
kongbyoungwook <kbw7777777@naver.com>
2019-10-08 23:04:41 +0900

    log test

--- src/main/java/com/example/demo/MyPetApplication.java
+++ src/main/java/com/example/demo/MyPetApplication.java
@@ -27,7 +27,7 @@ public class MyPetApplication {
 
 	//TODO 게시글 등록하는 api 
 	//TODO 스웨거 붙여보기
-
+	//
 	// @Bean
 	// public CommandLineRunner runner(BoardRepository boardRepository) {
 	// 	return (args) -> {
